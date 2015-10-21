f86ba2f6210416ada651cf81bb74a9ae937c5d0d
mdclement <mike@softwareontheside.com>
2015-10-21 16:17:19 -0400

    improving the branching koans

--- koans/AboutBranching.js
+++ koans/AboutBranching.js
@@ -7,6 +7,14 @@ describe("About Branching", function() {
     expect(value).toBe(FILL_ME_IN);
   });
 
+  it("should not execute the code inside the if", function() {
+    var value = 5;
+    if (value > 10) {
+      value += 5;
+    }
+    expect(value).toBe(FILL_ME_IN);
+  });
+
   it("should help you see how if statements work", function() {
     var value = 1;
     if (value > FILL_ME_IN) {
@@ -15,16 +23,30 @@ describe("About Branching", function() {
     expect(value).toBe(4);
   });
 
-  it("should help you see how if statements work", function() {
+  it("should help you see how if/else statements work", function() {
     var funValue;
     var funLevel = 8;
-    if (funLevel > 5) {
-      funValue = "Fun";
+    if (funLevel > 9) {
+      funValue = "Super Fun";
     }
     else {
-      funValue = "meh";
+      funValue = "Fun";
     }
     expect(funValue).toBe(FILL_ME_IN);
   });
 
+  it("should help you see how if/elseif/else statements work", function() {
+    var funValue;
+    var funLevel = 8;
+    if (funLevel > 9) {
+      funValue = "Super Fun";
+    }
+    else if (funLevel > 5) {
+      funValue = "Fun";
+    }
+    else {
+      funValue = "Meh";
+    }
+    expect(funValue).toBe(FILL_ME_IN);
+  });
 });
